17d9a85a9725a3cdb1f360ff9a413235fca8104e
mariosk <marios.koulakis@gmail.com>
2017-06-13 20:10:40 -0400

    Parses the weblog lines.

--- build.sbt
+++ build.sbt
@@ -2,5 +2,10 @@ name := "WeblogChallenge"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
-        
+scalaVersion := "2.11.11"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.1.1",
+  "org.apache.spark" %% "spark-sql" % "2.1.1",
+  "org.scalatest" %% "scalatest" % "3.0.3" % "test"
+)
--- src/main/scala/WeblogChallenge.scala
+++ src/main/scala/WeblogChallenge.scala
@@ -1,6 +1,21 @@
 /**
   * Created by mariosk on 12/06/17.
   */
+import org.apache.spark.sql.{Row, SparkSession}
+
 object WeblogChallenge extends App {
-  println("hello world")
+  val spark = SparkSession.builder().appName("Weblog Challenge").master("local[2]").getOrCreate()
+
+  def parseLine(line: String): Row = {
+    val logsPattern = """^(\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) \"([^\"]+)\" \"([^\"]+)\" (\S+) (\S+)""".r
+    val a:AnyVal = 4
+    line match {
+      case logsPattern(a @ _*) => Row.fromSeq(a)
+    }
+  }
+
+  val weblogs =
+    spark.sparkContext
+      .textFile("data/2015_07_22_mktplace_shop_web_log_sample.log")
+      .map(parseLine)
 }
