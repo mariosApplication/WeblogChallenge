c40247633bff3144accbd44da4a5e0a0f477433b
mariosk <marios.koulakis@gmail.com>
2017-06-14 01:46:49 -0400

    Extracts the sessions without an inactive window.

--- src/main/scala/WeblogChallenge.scala
+++ src/main/scala/WeblogChallenge.scala
@@ -6,7 +6,9 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
-object WeblogChallenge extends Serializable {
+import scala.math.BigDecimal.{double2bigDecimal, long2bigDecimal}
+
+object WeblogChallenge extends Serializable with App {
   def getWeblogs(context: SparkContext, filepath: String): RDD[LogLine] =
     context
       .textFile(filepath)
@@ -20,8 +22,16 @@ object WeblogChallenge extends Serializable {
       .getOrCreate()
       .sparkContext
 
+  def extractSessions(logs: RDD[LogLine]): RDD[(String, BigDecimal)] =
+    logs
+      .groupBy(log => log.client_port)
+      .map { case (ip, relevantLogs) => {
+        val timestamps = relevantLogs.map(l => long2bigDecimal(l.timestamp) / double2bigDecimal(60000.0))
+        (ip, timestamps.max - timestamps.min)
+      }}
+
   def test() =
-    getWeblogs(defaultContext(), "data/2015_07_22_mktplace_shop_web_log_sample.log")
-      .take(3)
+    extractSessions(getWeblogs(defaultContext(), "data/2015_07_22_mktplace_shop_web_log_sample.log"))
+      .take(10)
       .foreach(println)
 }
