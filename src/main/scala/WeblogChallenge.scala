2683ca26afa32ca9da75daf1728bc70fb57b638d
mariosk <marios.koulakis@gmail.com>
2017-06-13 23:17:48 -0400

    Extracts weblogs extraction to separate function.

--- src/main/scala/WeblogChallenge.scala
+++ src/main/scala/WeblogChallenge.scala
@@ -1,15 +1,27 @@
 /**
   * Created by mariosk on 12/06/17.
   */
-import PreprocessData.parseLine
+import PreprocessData.{LogLine, parseLine}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object WeblogChallenge extends App {
-  val spark = SparkSession.builder().appName("Weblog Challenge").master("local[2]").getOrCreate()
+  def getWeblogs(context: SparkContext, filepath: String): RDD[LogLine] = {
+    context
+      .textFile(filepath)
+      .map(parseLine)
+  }
 
-  val lines = spark.sparkContext.textFile("data/2015_07_22_mktplace_shop_web_log_sample.log")
-  val dataset = lines.map(parseLine)
+  def defaultContext() =
+    SparkSession
+      .builder()
+      .appName("Weblog Challenge")
+      .master("local[2]")
+      .getOrCreate()
+      .sparkContext
 
-  dataset.take(3).foreach(println)
-  spark.close()
+  getWeblogs(defaultContext(), "data/2015_07_22_mktplace_shop_web_log_sample.log")
+    .take(3)
+    .foreach(println)
 }
--- src/test/scala/WeblogChallengeSpec.scala
+++ src/test/scala/WeblogChallengeSpec.scala
@@ -1,7 +1,7 @@
 /**
   * Created by mariosk on 13/06/17.
   */
-import PreprocessData.{LogLine, parseLine}
+import WeblogChallenge.getWeblogs
 import org.apache.spark.sql.SparkSession
 import org.scalatest._
 
@@ -20,16 +20,14 @@ class WeblogChallengeSpec extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   "WeblogChallenge" should "parse a weblog file" in {
-    val input = spark.sparkContext.textFile("src/test/resources/weblogs_first_3_lines.log")
     val expected = Array(
       Array("2015-07-22T09:00:28.019143Z", "GET https://paytm.com:443/shop/authresponse?code=f2405b05-e2ee-4b0d-8f6a-9fed0fcfe2e0&state=null HTTP/1.1"),
       Array("2015-07-22T09:00:27.894580Z", "GET https://paytm.com:443/shop/wallet/txnhistory?page_size=10&page_number=0&channel=web&version=2 HTTP/1.1"),
       Array("2015-07-22T09:00:27.885745Z", "GET https://paytm.com:443/shop/wallet/txnhistory?page_size=10&page_number=0&channel=web&version=2 HTTP/1.1"))
 
-    val result = input
-        .map(
-          ((l: LogLine) => Array(l.timestamp, l.request))
-            .compose(parseLine))
+    val result =
+      getWeblogs(spark.sparkContext, "src/test/resources/weblogs_first_3_lines.log")
+        .map(l => Array(l.timestamp, l.request))
         .take(3)
 
     result should be(expected)
