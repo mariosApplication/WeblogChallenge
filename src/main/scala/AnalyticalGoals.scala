e2f296bf284d0d168bc5fd55b5f4f263f97a2301
mariosk <marios.koulakis@gmail.com>
2017-06-15 21:54:31 -0400

    Extracts the main tasks logic to separate file.

---
+++ src/main/scala/AnalyticalGoals.scala
@@ -0,0 +1,42 @@
+/**
+  * Created by mariosk on 15/06/17.
+  */
+import ExtractSessions.{Session, extractSessions}
+import ReadData.{defaultContext, getWeblogs}
+import SessionProcessing.getSessionLenght
+import org.apache.spark.rdd.RDD
+
+object AnalyticalGoals {
+  def getSessionsPerIP(filepath: String): RDD[(String, Seq[Session])] =
+    extractSessions()(getWeblogs(defaultContext(), filepath))
+      .collect{ case (ip, Some(ipSessions)) => (ip, ipSessions)}
+
+  def allSessions(sessionsPerIp: RDD[(String, Seq[Session])]): RDD[Session] =
+    sessionsPerIp.flatMap(_._2)
+
+  def getAverageSessionTime(sessions: RDD[Session]): Double =
+    sessions
+      .filter(_.length >= 2)
+      .map(getSessionLenght)
+      .mean()
+
+  private[this] def parseUrlFromRequest(request: String) : String = {
+    val pattern = """^\S+ (\S+:\/\/\S+):\S+ \S+$""".r
+    request match { case pattern(url) => url }
+  }
+
+  def getUniqueUrlVisits(sessions: RDD[Session]): RDD[(Session, List[String])] =
+    sessions
+      .map(session =>
+        (session,
+         session
+           .map(log => parseUrlFromRequest(log.request))
+           .distinct))
+
+  def getMostEngagedUsers(sessionsPerIp: RDD[(String, Seq[Session])]): RDD[(String, BigDecimal)] =
+    sessionsPerIp
+      .collect{
+        case (ip, ipSessions) => (ip, ipSessions.map(getSessionLenght).max)
+      }
+      .sortBy(-_._2)
+}
--- src/main/scala/WeblogChallenge.scala
+++ src/main/scala/WeblogChallenge.scala
@@ -1,67 +1,18 @@
 /**
   * Created by mariosk on 12/06/17.
   */
-import ReadData.{getWeblogs, defaultContext}
-import ExtractSessions.extractSessions
-import SessionProcessing.getSessionLenght
+import AnalyticalGoals.{getSessionsPerIP, allSessions, getAverageSessionTime, getUniqueUrlVisits, getMostEngagedUsers}
 
 object WeblogChallenge extends Serializable with App {
-  val sessionsPerIP = extractSessions()(getWeblogs(defaultContext(), "data/2015_07_22_mktplace_shop_web_log_sample.log"))
+  val dataFilepath = "data/2015_07_22_mktplace_shop_web_log_sample.log"
 
-  val sessions = sessionsPerIP
-    .flatMap{
-      case (_, Some(ipSessions)) => ipSessions
-      case _ => Seq()
-    }
+  val sessionsPerIP = getSessionsPerIP(dataFilepath)
 
-  /// AVERAGE SESSION TIME ///
-  val averageSessionTime =
-    sessions
-      .filter(_.length >= 2)
-      .map(getSessionLenght)
-      .mean()
+  val sessions = allSessions(sessionsPerIP)
 
-  println(s"Average session time: ${averageSessionTime * 60.0} seconds")
-  // Averate session time: 38.03842136008329 seconds -- excluding all 0 time length sessions
-  // Average session time: 37.95641017922952 seconds -- excluding single log sessions
-  // Average session time: 14.487383547154677 seconds -- including single log sessions with 0 time length
+  val averageSessionTime = getAverageSessionTime(sessions)
 
+  val uniqueUrlVisits = getUniqueUrlVisits(sessions)
 
-  /// UNIQUE URL VISITS PER SESSION ///
-  def parseUrlFromRequest(request: String) : String = {
-    val pattern = """^\S+ (\S+:\/\/\S+):\S+ \S+$""".r
-    request match { case pattern(url) => url }
-  }
-
-  val uniqueUrlVisits =
-    sessions
-      .map(session =>
-        (session,
-         session
-           .map(log => parseUrlFromRequest(log.request))
-           .distinct))
-
-  println(s"Number of unique url visits: ${uniqueUrlVisits.map(_._2.length).sum()}")
-  // Number of unique url visits: 438886.0
-
-  /// MOST ENGAGED USERS ///
-  val mostEngagedUsers =
-    sessionsPerIP
-      .collect{
-        case (ip, Some(ipSessions)) => (ip, ipSessions.map(getSessionLenght).max)
-      }
-      .sortBy(-_._2)
-
-  println(s"Those are the most engaged ip addresses: ${mostEngagedUsers.take(10).foreach(println)}")
-  // Weird... The lengths seem to be very close...
-  //  (213.239.204.204:35094,34.42645)
-  //  (103.29.159.138:57045,34.42343333333333333333333333)
-  //  (203.191.34.178:10400,34.42243333333333333333333333)
-  //  (78.46.60.71:58504,34.41436666666666666666666666)
-  //  (54.169.191.85:15462,34.36146666666666666666666667)
-  //  (103.29.159.186:27174,34.34790000000000000000000000)
-  //  (122.169.141.4:11486,34.3355)
-  //  (122.169.141.4:50427,34.31826666666666666666666666)
-  //  (103.29.159.62:55416,34.31655000000000000000000000)
-  //  (103.29.159.213:59453,34.30885000000000000000000000)
+  val mostEngagedUsers = getMostEngagedUsers(sessionsPerIP)
 }
